f0c5998ded2f57a306b39d89332626a122bfd667
chiboycalix <igwechinonso77@gmail.com>
2021-02-16 21:23:47 +0100

    fix error on build

--- src/Map.js
+++ src/Map.js
@@ -1,22 +1,22 @@
 /** @format */
 
 import React from 'react';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { Map, TileLayer } from 'react-leaflet';
 import './map.css';
 import { showDataOnMap } from './util';
 
-const Map = ({ countries, casesType, center, zoom }) => {
+const MapC = ({ countries, casesType, center, zoom }) => {
 	console.log({ casesType });
 	return (
 		<div className="map">
-			<MapContainer className="markercluster-map" center={center} zoom={zoom}>
+			<Map className="markercluster-map" center={center} zoom={zoom}>
 				<TileLayer
 					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 					attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 				/>
 				{showDataOnMap(countries, casesType)}
-			</MapContainer>
+			</Map>
 		</div>
 	);
 };
-export default Map;
+export default MapC;
